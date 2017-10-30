8d2471c6cea476fbcaeafde391c37a80b616ed53
maheshbailwal <Mahesh.Bailwal@rsystems.com>
2017-10-30 12:51:53 +0530

    moved code to github from local system

---
+++ Compiler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace CoffeeEditor
+{
+    public class CoffeCompiler : ICompiler
+    {
+        public string Complie(string coffeText)
+        {
+            string javaScriptText;
+            string tempFile = Path.Combine(Directory.GetCurrentDirectory(), Guid.NewGuid()+"temp.coffee");
+   
+            try
+            {
+                File.WriteAllText(tempFile, coffeText);
+                javaScriptText = ExeRunner.Execute(Helper.GetCoffeePath(), @"--compile --print " + tempFile);
+                File.Delete(tempFile);
+            }
+            catch (ExeExecutionException ex)
+            {
+                throw;
+            }
+            return javaScriptText;
+        }
+
+    }
+}
---
+++ ExeRunner.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace CoffeeEditor
+{
+    /// <summary>
+    ///     Class for running exe in seprate proccess
+    /// </summary>
+    public static class ExeRunner
+    {
+        public static string Execute(string fullExePath, string exeParams)
+        {
+            var output = string.Empty;
+            var exeProcessStartInfo = new ProcessStartInfo(fullExePath, exeParams)
+                                          {
+                                              WorkingDirectory =
+                                                  Path.GetDirectoryName(
+                                                      fullExePath), 
+                                              CreateNoWindow = true, 
+                                              UseShellExecute = false, 
+                                              ErrorDialog = false, 
+                                              RedirectStandardError = true, 
+                                              RedirectStandardOutput = true
+                                          };
+
+            var exeProcess = new Process();
+            exeProcess.EnableRaisingEvents = true;
+            exeProcess.StartInfo = exeProcessStartInfo;
+            exeProcess.Start();
+
+            StreamReader stdReader = exeProcess.StandardOutput;
+            output = stdReader.ReadToEnd();
+
+            StreamReader errReader = exeProcess.StandardError;
+            string errOut = errReader.ReadToEnd();
+            exeProcess.WaitForExit();
+
+            var errors = errOut != null && errOut.Length > 0 ? errOut : string.Empty;
+
+            if (exeProcess.HasExited)
+            {
+                exeProcess.Close();
+            }
+            else
+            {
+                exeProcess.Kill();
+            }
+
+            exeProcess.Dispose();
+
+            if (!string.IsNullOrEmpty(errors))
+            {
+                throw new ExeExecutionException(errors, fullExePath, exeParams);
+            }
+
+            return output;
+        }
+    }
+}
